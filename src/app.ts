294b131e27c263256a6d4f7b416f044a783b0405
Duc Nguyen <minhduc140583@gmail.com>
2026-08-22 01:10:46 +0700

    Upgrade code

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import path from "path"
 import { Pool } from "pg"
 import { ExportService, select, Statement } from "pg-exporter"
 import { config, environments } from "./config"
-import { User, userModel } from "./user"
+import { User, userModel as userSchema } from "./user"
 
 const cfg = merge(config, process.env, environments, process.env.ENV)
 
@@ -14,31 +14,31 @@ export class QueryBuilder {
     this.buildQuery = this.buildQuery.bind(this)
   }
   buildQuery(cxt?: any): Promise<Statement> {
-    const stmt: Statement = { query: select("export_users", userModel) }
+    const stmt: Statement = { query: select("export_users", userSchema) }
     return Promise.resolve(stmt)
   }
 }
 
 async function exportData() {
   const now = new Date()
-  const errorWriter = new LogWriter(getPrefix(cfg.error.prefix, now) + "_" + timeToString(now) + cfg.error.suffix, cfg.error.directory)
-  const logWriter = new LogWriter(getPrefix(cfg.info.prefix, now) + "_" + timeToString(now) + cfg.info.suffix, cfg.info.directory)
+  const errorWriter = new LogWriter(`${getPrefix(cfg.error.prefix, now)}_${timeToString(now)}${cfg.error.suffix}`, cfg.error.directory)
+  const logWriter = new LogWriter(`${getPrefix(cfg.info.prefix, now)}_${timeToString(now)}${cfg.info.suffix}`, cfg.info.directory)
 
   const logger = createFileLogger(cfg.log, errorWriter.write, logWriter.write)
 
+  const pool = new Pool(cfg.db)
+  const queryBuilder = new QueryBuilder()
+  const formatter = new CSVFormatter<User>(userSchema, ",")
+
   const dir = cfg.file.path
-  const filename = getPrefix(cfg.file.prefix, now) + "_" + timeToString(now) + ".csv"
+  const filename = `${getPrefix(cfg.file.prefix, now)}_${timeToString(now)}.csv`
   const writeStream = createWriteStream(dir, filename)
   const writer = new FileWriter(writeStream)
-  const pool = new Pool(cfg.db)
-
-  const formatter = new CSVFormatter<User>(userModel, ",")
-  const queryBuilder = new QueryBuilder()
 
   try {
     logger.info(`Start to export "${path.join(dir, filename)}" file`)
     writer.write(cfg.file.header)
-    const exporter = new ExportService<User>(pool, filename, queryBuilder, formatter, writer, userModel, logger.info, 10000)
+    const exporter = new ExportService<User>(pool, filename, queryBuilder, formatter, writer, userSchema, logger.info, 10000)
     const total = await exporter.export()
 
     logger.info(`Export "${path.join(dir, filename)}" file. Total: ${total}`)
@@ -46,9 +46,7 @@ async function exportData() {
     logger.error(`Error when export "${path.join(dir, filename)}" file. Details: ${toString(err)}`)
   } finally {
     await pool.end()
-    errorWriter.flush()
     errorWriter.end()
-    logWriter.flush()
     logWriter.end()
   }
 }
